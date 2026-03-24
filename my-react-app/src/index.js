4805a5f38140a6975eeae89e0548a61ca3f118f9
Zenkai-Jay <JESURUN@email.sc.edu>
2026-03-23 21:52:14 -0400

    work

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './pages/Home';
@@ -8,13 +8,13 @@ import Recipes from './pages/Recipes';
 
 export default function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="recipes" element={<Recipes />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
